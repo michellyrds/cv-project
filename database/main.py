94f1e201b6a4c9de658bc620eefa8cb697c30447
Pedro Santana <panjos@uolinc.com>
2022-06-25 20:08:49 -0300

    adding .env

--- database/main.py
+++ database/main.py
@@ -1,16 +1,14 @@
 def get_database():
+    from dotenv import load_dotenv
     from pymongo import MongoClient
+    import os
     import pymongo
 
-    CONNECTION_STRING = "mongodb+srv://<username>:<password>@cv-project.btad0l1.mongodb.net/?retryWrites=true&w=majority"
+    load_dotenv()
+
+    CONNECTION_STRING = "mongodb+srv://" + os.getenv("USERNAME") + ":" + os.getenv("PASSWORD") + "@cv-project.btad0l1.mongodb.net/?retryWrites=true&w=majority"
 
     client = pymongo.MongoClient(CONNECTION_STRING)
     db = client.test
-    print(db)
     
     return client['screen_time']
-
-if __name__ == "__main__":
-
-    dbname = get_database()
-    print(dbname)
