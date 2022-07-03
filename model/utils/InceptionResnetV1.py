9215ca9fd1848132e98c561ae267a3fbc9854771
ThewMax <123mateus123@gmail.com>
2022-07-03 16:01:37 -0300

    fix: import utils on resnet fixed

--- model/utils/InceptionResnetV1.py
+++ model/utils/InceptionResnetV1.py
@@ -9,7 +9,7 @@ from requests.adapters import HTTPAdapter
 import torch
 from torch import nn
 from torch.nn import functional as F
-from utils.download import download_url_to_file
+from model.utils.download import download_url_to_file
 
 class BasicConv2d(nn.Module):
 
